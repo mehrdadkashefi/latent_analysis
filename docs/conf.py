61f45629dd57cf4d0fd815a174f9402020330636
Mehrdad Kashefi <kashefi.mehrdad@yahoo.com>
2025-01-02 16:35:49 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../PcmPy'))
+sys.path.insert(0, os.path.abspath('../latent_analysis'))
 
 # -- Project information -----------------------------------------------------
 
